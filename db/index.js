9f915310287eb6032f919a8090782c8ab6fcfa4c
Kelsey Benedict <kelseybenedict26@gmail.com>
2021-05-04 20:31:18 -0600

    added functionality to add new employee

--- db/index.js
+++ db/index.js
@@ -15,11 +15,30 @@ class DB {
     
     // find all departments
     findAllDepartments(){
+        // left joins to update dept table
         return this.connection.query("SELECT department.id, department.name FROM department LEFT JOIN roles ON roles.department_id = department_id LEFT JOIN employee ON employee.role_id = role_id GROUP BY department.id, department.name")
-    }
+    };
+    // adds a department to department table
     addDepartment(department){
         return this.connection.query("INSERT INTO department SET ?", department);
+    };
+    // find all employees
+    findAllEmployees(){
+        // join with roles table and departments table to display
+        // employees' salaries, roles, depts, and managers
+        return this.connection.query("SELECT employee.id, employee.first_name, employee.last_name, roles.title, department.name AS department, roles.salary FROM employee LEFT JOIN roles on employee.role_id = roles.id LEFT JOIN department on roles.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id;"
+        );
+    };
+    // find all roles
+    findAllRoles(){
+        // join all roles with department to show dept name
+        return this.connection.query("SELECT roles.id, roles.title, department.name AS department, roles.salary FROM roles LEFT JOIN department on roles.department_id = department.id;")
+    };
+    // add new employee to db
+    addEmployee(newEmployee){
+        return this.connection.query("INSERT INTO employee SET ?", newEmployee)
     }
+
 };
 
 module.exports = new DB(connection);
--- employeeTracker.js
+++ employeeTracker.js
@@ -10,16 +10,16 @@ const { exit } = require("process");
 displayLogo();
 
 // function to display "employee tracker logo"
-function displayLogo(){
+function displayLogo() {
     // setting the text to display
-    const logoText = logo({name : "Employee Tracker"}).render();
+    const logoText = logo({ name: "Employee Tracker" }).render();
     // logging it so it's visible on the command line
     console.log(logoText);
     // calling function to display questions
     init()
 }
 
-function init () {
+function init() {
     // initial question options
     inquirer.prompt([
         {
@@ -27,47 +27,47 @@ function init () {
             name: 'task',
             message: 'What would you like to do?',
             // left to add: remove a employee, dept, or role, update an employee manager, view employees by manager 
-            choices: ['Add role', 'Add department', 'Add new employee', "Update an employee's role",'View all employees', 'View all departments', 'View all roles', 'Exit']
+            choices: ['Add role', 'Add department', 'Add new employee', "Update an employee's role", 'View all employees', 'View all departments', 'View all roles', 'Exit']
         },
 
-       
-    ])
-    .then((data) => {
-        // using a switch to call the appropriate function depending on what the user selected
-        switch (data.task){
-            case "Add role":
-                addRole();
-                break;
-            case "Add department":
-                addDepartment();
-                break;
-            case "Add new employee":
-                addEmployee();
-                break;
-            case "Update an employee's role":
-                updateRole();
-                break;
-            case "View all employees":
-                viewEmployees();
-                break;
-            case "View all departments":
-                viewDepartments();
-                break;
-            case "View all roles":
-                viewRoles();
-                break;
-            // if none of the above cases were selected, this will exit out of the established connection
-            default:
-                db.connection.end();
-        }
 
-    })
+    ])
+        .then((data) => {
+            // using a switch to call the appropriate function depending on what the user selected
+            switch (data.task) {
+                case "Add role":
+                    addRole();
+                    break;
+                case "Add department":
+                    addDepartment();
+                    break;
+                case "Add new employee":
+                    addEmployee();
+                    break;
+                case "Update an employee's role":
+                    updateRole();
+                    break;
+                case "View all employees":
+                    viewEmployees();
+                    break;
+                case "View all departments":
+                    viewDepartments();
+                    break;
+                case "View all roles":
+                    viewRoles();
+                    break;
+                // if none of the above cases were selected, this will exit out of the established connection
+                default:
+                    db.connection.end();
+            }
+
+        })
 }
 
 async function addRole() {
     // finding all departments in database
     const departments = await db.findAllDepartments();
-    const deptChoices = departments.map(({id, name}) => ({
+    const deptChoices = departments.map(({ id, name }) => ({
         name: name,
         value: id
     }))
@@ -77,18 +77,18 @@ async function addRole() {
             type: "input",
             name: "title",
             message: "What is the name of the role?"
-          },
-          {
+        },
+        {
             type: "input",
             name: "salary",
             message: "What is the salary of the role?"
-          },
-          {
+        },
+        {
             type: "list",
             name: "department_id",
             message: "Which department does this role belong to?",
             choices: deptChoices
-          }
+        }
     ])
     await db.addRole(role)
     // success response
@@ -96,21 +96,78 @@ async function addRole() {
     // display initial questions again
     init()
 
-}
+};
 
 // function to add department 
-async function addDepartment(){
+async function addDepartment() {
     // prompt for adding dept
     const department = await inquirer.prompt([
         {
             type: "input",
             name: "name",
             message: "What is the name of the department?"
-          }
+        }
     ])
     // adding dept to database
     await db.addDepartment(department)
     console.log(`Success! Added ${department.name} to the database`);
     // display initial questions again
     init()
+}
+
+// function to add new employee
+async function addEmployee() {
+    // pulling all employees and all roles in the database
+    // so that we can assign a new employee an existing role
+    // and include the new employee into an updated all roles table
+    const allEmployees = await db.findAllEmployees();
+    const allRoles = await db.findAllRoles();
+    // create role options
+    const roleOptions = allRoles.map(({ id, title }) => ({
+        name: title,
+        value: id
+    }));
+    // create manager options
+    const managerOptions = allEmployees.map(({ id, first_name, last_name }) => ({
+        name: `${first_name} ${last_name}`,
+        value: id
+    }));
+    // allowing a manager option to be null, if the new employee is considereed a manager 
+    managerOptions.unshift({ name: "None", value: null })
+    // new employee questions
+    const newEmployee = await inquirer.prompt([
+        {
+            type: "input",
+            name: "first_name",
+            message: "What is the employee's first name?"
+        },
+        {
+            type: "input",
+            name: "last_name",
+            message: "What is the employee's last name?"
+        },
+        {
+            type: "list",
+            name: "role_id",
+            message: "What is the employee's role?",
+            choices: roleOptions
+        },
+        {
+            type: "list",
+            name: "manager_id",
+            message: "Who is the employee's manager?",
+            choices: managerOptions
+        }
+    ]);
+    await db.addEmployee(newEmployee);
+    console.log(`Success! Added ${newEmployee.first_name} ${newEmployee.last_name} to the database`);
+    // display initial questions again
+    init();
+}
+
+// function to update employee's role
+async function updateRole() {
+
+    const allEmployees = await db.findAllEmployees();
+
 }
